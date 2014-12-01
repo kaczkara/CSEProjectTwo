169aabddbab326f3eff2bd6a19b538975d2f2c40
Rachel Ania <rakaczka@gmail.com>
2014-12-01 17:41:52 -0500

    Christmas tree/gitignore update?

--- OpenGlCse386/Project2/ChristmasTree.cpp
+++ OpenGlCse386/Project2/ChristmasTree.cpp
@@ -7,7 +7,7 @@ ChristmasTree::ChristmasTree() {
 	trunk->fixedTransformation = rotate(translate(mat4(1.0f), vec3(0.0f,0.0f,0.0f)),90.0f,vec3(1.0,0.0,0.0));
 	trunk->material.setAmbientAndDiffuseMat(vec4(0.4f, 0.2f, 0.0f, 1.0f));
 	trunk->material.setEmissiveMat(vec4(0.2f, 0.2f, 0.2f, 0.5f));
-	this->addChild(trunk);
+	this->addChild(trunk); 
 	
 	VisualObject* base = new Cone( 2.0f);
 	base->fixedTransformation = rotate(translate(mat4(1.0f), vec3(0.0f, 0.0f, 0.0f)), -90.0f, vec3(1.0f, 0.0f, 0.0f));
